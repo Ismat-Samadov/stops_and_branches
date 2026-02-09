242806d2998320a5721617e49c12d193d1cb50b7
Ismat-Samadov <ismetsemedov@gmail.com>
2026-02-09 12:23:41 +0400

    init

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import TimezoneConverter from '@/components/TimezoneConverter';
-
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800 py-8 px-4">
-      <TimezoneConverter />
-    </div>
-  );
-}
